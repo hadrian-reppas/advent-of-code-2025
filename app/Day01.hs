7cfe8a9fa97c621d74c58a84efebac6e7ef5aa85
Hadrian <hadrian@uchicago.edu>
2025-12-06 20:52:31 -0600

    day 1

---
+++ app/Day01.hs
@@ -0,0 +1,26 @@
+module Day01
+  ( part1,
+    part2,
+  )
+where
+
+parseMove :: String -> Int
+parseMove ('R' : n) = read n
+parseMove ('L' : n) = -read n
+
+countZeros :: Int -> [String] -> Int
+countZeros _ [] = 0
+countZeros pos (move : moves) =
+  let newPos = (pos + parseMove move) `mod` 100
+      tailCount = countZeros newPos moves
+   in if newPos == 0 then 1 + tailCount else tailCount
+
+part1 :: String -> Int
+part1 = countZeros 50 . lines
+
+expand :: [String] -> [String]
+expand [] = []
+expand ((c : n) : moves) = replicate (read n) (c : "1") ++ expand moves
+
+part2 :: String -> Int
+part2 = countZeros 50 . expand . lines
---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Day01 qualified
+
+run :: (Show a, Show b) => Int -> (String -> a) -> (String -> b) -> IO ()
+run day part1 part2 = do
+  input <- readFile $ "input/day" ++ show day ++ ".txt"
+  putStrLn $ "part1: " ++ show (part1 input)
+  putStrLn $ "part2: " ++ show (part2 input)
+
+main :: IO ()
+main = do
+  run 1 Day01.part1 Day01.part2
