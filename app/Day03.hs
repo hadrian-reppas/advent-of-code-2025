2edf156c898700b40e67abb456c2af8ed2672854
Hadrian <hadrian@uchicago.edu>
2025-12-07 06:54:04 -0600

    day 3

---
+++ app/Day03.hs
@@ -0,0 +1,22 @@
+module Day03
+  ( part1,
+    part2,
+  )
+where
+
+import Data.Char (digitToInt)
+import Data.Function (on)
+import Data.List (maximumBy)
+
+maxJoltage :: Int -> String -> Int
+maxJoltage 0 _ = 0
+maxJoltage n s =
+  let cmp = compare `on` \t -> (snd t, -fst t)
+      (i, d) = maximumBy cmp $ zip [1 .. length s - n + 1] s
+   in 10 ^ (n - 1) * digitToInt d + maxJoltage (n - 1) (drop i s)
+
+part1 :: String -> Int
+part1 = sum . map (maxJoltage 2) . lines
+
+part2 :: String -> Int
+part2 = sum . map (maxJoltage 12) . lines
--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,17 @@ module Main where
 
 import Day01 qualified
 import Day02 qualified
+import Day03 qualified
 
 run :: (Show a, Show b) => Int -> (String -> a) -> (String -> b) -> IO ()
 run day part1 part2 = do
   input <- readFile $ "input/day" ++ show day ++ ".txt"
-  putStrLn $ "part1: " ++ show (part1 input)
-  putStrLn $ "part2: " ++ show (part2 input)
+  putStrLn $ "day " ++ show day ++ ":"
+  putStrLn $ "  part 1: " ++ show (part1 input)
+  putStrLn $ "  part 2: " ++ show (part2 input)
 
 main :: IO ()
 main = do
   run 1 Day01.part1 Day01.part2
   run 2 Day02.part1 Day02.part2
+  run 3 Day03.part1 Day03.part2
